ddf6eeb8cf7ac2ddce02d927529431edade7084b
irvingrivas <irving0594@gmail.com>
2018-10-07 22:22:10 -0700

    Input Validation

--- index.js
+++ index.js
@@ -7,23 +7,42 @@ var randWordList = ["router", "pencil","computer","love","genius","antidisestabl
     "raining", "sunny", "Philidelphia", "Washington"];
 var randWord = randWordList[Math.floor(Math.random() * randWordList.length)];
 var word = new Word(randWord);
+var numGuesses = 10;
 
 function init() {
-    inquirer
-        .prompt({
-            name: "letter",
-            type: "input",
-            message: "What letter would you like to guess?\n" + word.getString() + "\n"
-        })
-        .then(function(answer) {
-            word.checkLetters(answer.letter);
-            console.log(word.getString());
-            if (word.getString() === randWord) {
-                console.log("The word is " + randWord + ". You win!");
-            } else {
-                init();
-            }
-        })
+    if (numGuesses > 0) {
+        inquirer
+            .prompt({
+                name: "letter",
+                type: "input",
+                message: "What letter would you like to guess?\n" + word.getString() + "\n"
+            })
+            .then(function(answer) {
+                answer.letter = answer.letter.toLowerCase();
+                if (/[^a-zA-Z]/.test(answer.letter)) {
+                    console.log("Not a letter!");
+                    ++numGuesses;
+                    init();
+                }
+                word.checkLetters(answer.letter);
+                if (answer.letter.length > 1 ) {
+                    console.log("One letter inputs only!");
+                    ++numGuesses;
+                    init();
+                }
+                console.log(word.getString());
+                if (word.getString() === randWord) {
+                    console.log("The word is " + randWord + ". You win!");
+                } else {
+                    --numGuesses;
+                    console.log("You have " + numGuesses + " guesses left.\n");
+                    init();
+                }
+            })
+    } else {
+        console.log("The word is " + randWord + ". You lose :(")
+    }
+
 }
 
 init();
